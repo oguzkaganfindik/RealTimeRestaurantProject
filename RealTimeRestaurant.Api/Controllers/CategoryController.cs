80f86dfa20898773f99cb5f7a2cdd443ebfd43bf
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-06 01:09:43 +0300

    Kategori Silme Ve Guncelleme Islemleri Yapildi

--- RealTimeRestaurant.Api/Controllers/CategoryController.cs
+++ RealTimeRestaurant.Api/Controllers/CategoryController.cs
@@ -38,7 +38,7 @@ namespace RealTimeRestaurant.Api.Controllers
             return Ok("Kategori Eklendi");
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteCategory(int id) 
         {
             var value = _categoryService.TGetById(id);
@@ -46,7 +46,7 @@ namespace RealTimeRestaurant.Api.Controllers
             return Ok("Kategori Silindi");
         }
 
-        [HttpGet("GetCategory")]
+        [HttpGet("{id}")]
         public IActionResult GetCategory(int id) 
         {
             var value = _categoryService.TGetById(id);
