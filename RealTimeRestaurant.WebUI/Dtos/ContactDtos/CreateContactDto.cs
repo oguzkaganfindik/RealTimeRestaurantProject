1492ed16b651b148ef1b20f43c108c427819acba
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-07 16:46:16 +0300

    Iletisim islemleri: Listeleme, Ekleme, Guncelleme, Silme Tamamlandi.

---
+++ RealTimeRestaurant.WebUI/Dtos/ContactDtos/CreateContactDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealTimeRestaurant.WebUI.Dtos.ContactDtos
+{
+    public class CreateContactDto
+    {
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string FooterDescription { get; set; }
+    }
+}
