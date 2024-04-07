67a8995a355c461d6687f25aba8cd91a2f623bab
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-07 20:38:16 +0300

    Indirimli Urun islemleri: Listeleme, Ekleme, Guncelleme, Silme.

---
+++ RealTimeRestaurant.WebUI/Dtos/DiscountDtos/CreateDiscountDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealTimeRestaurant.WebUI.Dtos.DiscountDtos
+{
+    public class CreateDiscountDto
+    {
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
