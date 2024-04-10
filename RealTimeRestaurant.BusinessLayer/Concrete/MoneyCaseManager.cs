bd719d2064ebf93d75ac5b37e422b0fea3b89842
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-11 01:08:18 +0300

    Money Case Tablosu Ve Money Case Istatistigi

---
+++ RealTimeRestaurant.BusinessLayer/Concrete/MoneyCaseManager.cs
@@ -0,0 +1,46 @@
+﻿using RealTimeRestaurant.BusinessLayer.Abstract;
+using RealTimeRestaurant.DataAccessLayer.Abstract;
+using RealTimeRestaurant.EntityLayer.Entities;
+
+namespace RealTimeRestaurant.BusinessLayer.Concrete
+{
+    public class MoneyCaseManager : IMoneyCaseService
+    {
+        private readonly IMoneyCaseDal _moneyCaseDal;
+
+        public MoneyCaseManager(IMoneyCaseDal moneyCaseDal)
+        {
+            _moneyCaseDal = moneyCaseDal;
+        }
+
+        public void TAdd(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MoneyCase TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<MoneyCase> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal TTotalMoneyCaseAmount()
+        {
+            return _moneyCaseDal.TotalMoneyCaseAmount();
+        }
+
+        public void TUpdate(MoneyCase entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RealTimeRestaurant.EntityLayer/Entities/MoneyCase.cs
@@ -0,0 +1,8 @@
+﻿namespace RealTimeRestaurant.EntityLayer.Entities
+{
+    public class MoneyCase
+    {
+        public int MoneyCaseId { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
