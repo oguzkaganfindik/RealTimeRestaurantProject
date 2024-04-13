2f69ec19d2eca79ba7c50ad963114dd191aa833c
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-13 21:00:34 +0300

    Slider Api

---
+++ RealTimeRestaurant.BusinessLayer/Concrete/SliderManager.cs
@@ -0,0 +1,41 @@
+﻿using RealTimeRestaurant.BusinessLayer.Abstract;
+using RealTimeRestaurant.DataAccessLayer.Abstract;
+using RealTimeRestaurant.EntityLayer.Entities;
+
+namespace RealTimeRestaurant.BusinessLayer.Concrete
+{
+    public class SliderManager : ISliderService
+    {
+        private readonly ISliderDal _sliderDal;
+
+        public SliderManager(ISliderDal sliderDal)
+        {
+            _sliderDal = sliderDal;
+        }
+
+        public void TAdd(Slider entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Slider entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Slider TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Slider> TGetListAll()
+        {
+            return _sliderDal.GetListAll();
+        }
+
+        public void TUpdate(Slider entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
