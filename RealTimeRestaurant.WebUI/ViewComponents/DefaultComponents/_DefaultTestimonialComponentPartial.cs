d59a4369f0cf4b0974d9ff8e4f9fd7d263b911c8
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-14 19:56:29 +0300

    Hakkimizda Kismi ve Referanslar Kismi Api Consumelari

--- RealTimeRestaurant.WebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
+++ RealTimeRestaurant.WebUI/ViewComponents/DefaultComponents/_DefaultTestimonialComponentPartial.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealTimeRestaurant.WebUI.Dtos.TestimonialDtos;
 
 namespace RealTimeRestaurant.WebUI.ViewComponents.DefaultComponents
 {
     public class _DefaultTestimonialComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultTestimonialComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7265/api/Testimonial");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsonData);
+            return View(values);
+
+
         }
     }
 }
