5f643f1af0918144d1e8a234475e00d377143a16
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-18 19:23:51 +0300

    Sepet Ekrani Tasarimi Ve Sepette Urunlerin Listelenmesi

---
+++ RealTimeRestaurant.WebUI/Controllers/BasketController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealTimeRestaurant.WebUI.Dtos.BasketDtos;
+
+namespace RealTimeRestaurant.WebUI.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BasketController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7265/api/Basket?id=4");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBasketDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
