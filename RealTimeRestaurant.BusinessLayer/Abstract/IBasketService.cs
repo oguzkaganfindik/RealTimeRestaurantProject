3fa6430fa71a8f2a52428763b26a40ee693ebe25
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-18 14:14:28 +0300

    Basket Sinifinin Mimariye Entegre Edilmesi

---
+++ RealTimeRestaurant.BusinessLayer/Abstract/IBasketService.cs
@@ -0,0 +1,9 @@
+﻿using RealTimeRestaurant.EntityLayer.Entities;
+
+namespace RealTimeRestaurant.BusinessLayer.Abstract
+{
+    public interface IBasketService : IGenericService<Basket>
+    {
+        List<Basket> TGetBasketByMenuTableNumber(int id);
+    }
+}
---
+++ RealTimeRestaurant.BusinessLayer/Concrete/BasketManager.cs
@@ -0,0 +1,46 @@
+﻿using RealTimeRestaurant.BusinessLayer.Abstract;
+using RealTimeRestaurant.DataAccessLayer.Abstract;
+using RealTimeRestaurant.EntityLayer.Entities;
+
+namespace RealTimeRestaurant.BusinessLayer.Concrete
+{
+    public class BasketManager : IBasketService
+    {
+        private readonly IBasketDal _basketDal;
+
+        public BasketManager(IBasketDal basketDal)
+        {
+            _basketDal = basketDal;
+        }
+
+        public void TAdd(Basket entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Basket entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Basket> TGetBasketByMenuTableNumber(int id)
+        {
+            return _basketDal.GetBasketByMenuTableNumber(id);
+        }
+
+        public Basket TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Basket> TGetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Basket entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
