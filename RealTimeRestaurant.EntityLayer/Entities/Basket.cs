d34ce887c9a83477d4a42922e36702f4babb6648
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-18 13:18:46 +0300

    Sepet Sinifi

---
+++ RealTimeRestaurant.EntityLayer/Entities/Basket.cs
@@ -0,0 +1,14 @@
+﻿namespace RealTimeRestaurant.EntityLayer.Entities
+{
+    public class Basket
+    {
+        public int BasketId { get; set; }
+        public decimal Price { get; set; }
+        public decimal Count { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public int MenuTableId { get; set; }
+        public MenuTable MenuTable { get; set; }
+    }
+}
