49de0dc52561c84e6741a59756507b5629cf6e70
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-20 23:34:20 +0300

    Notification Sinifi

---
+++ RealTimeRestaurant.DataAccessLayer/EntityFramework/EfNotificationDal.cs
@@ -0,0 +1,14 @@
+﻿using RealTimeRestaurant.DataAccessLayer.Abstract;
+using RealTimeRestaurant.DataAccessLayer.Concrete;
+using RealTimeRestaurant.DataAccessLayer.Repositories;
+using RealTimeRestaurant.EntityLayer.Entities;
+
+namespace RealTimeRestaurant.DataAccessLayer.EntityFramework
+{
+    public class EfNotificationDal : GenericRepository<Notification>, INotificationDal
+    {
+        public EfNotificationDal(RealTimeRestaurantContext context) : base(context)
+        {            
+        }
+    }
+}
---
+++ RealTimeRestaurant.EntityLayer/Entities/Notification.cs
@@ -0,0 +1,11 @@
+﻿namespace RealTimeRestaurant.EntityLayer.Entities
+{
+    public class Notification
+    {
+        public int NotificationId { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
