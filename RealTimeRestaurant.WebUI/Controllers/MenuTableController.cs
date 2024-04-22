a52b8114a1453aaa6085949a8ffff924394ad7c5
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-22 22:39:46 +0300

    Masalarin Bos Ve Doluluk Durumlari

--- RealTimeRestaurant.WebUI/Controllers/MenuTableController.cs
+++ RealTimeRestaurant.WebUI/Controllers/MenuTableController.cs
@@ -87,5 +87,20 @@ namespace RealTimeRestaurant.WebUI.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> TableListByStatus()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7265/api/MenuTables");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMenuTableDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
     }
 }
