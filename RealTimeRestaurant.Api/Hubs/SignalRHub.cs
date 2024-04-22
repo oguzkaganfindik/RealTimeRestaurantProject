50c097cdffefadbd42e968ac00e0e0d2540c50d4
Oguzkagan Findik <oguzfindik14@gmail.com>
2024-04-22 23:26:20 +0300

    Masalarin Verilerinin SignalR ile Anlik Listelenerek Cekilmesi

--- RealTimeRestaurant.Api/Hubs/SignalRHub.cs
+++ RealTimeRestaurant.Api/Hubs/SignalRHub.cs
@@ -103,5 +103,11 @@ namespace RealTimeRestaurant.Api.Hubs
             var notificationListByFalse = _notificationService.TGetAllNotificationsByFalse();
             await Clients.All.SendAsync("ReceiveNotificationListByFalse", notificationListByFalse);
         }
+
+        public async Task GetMenuTableStatus()
+        {
+            var value = _menuTableService.TGetListAll();
+            await Clients.All.SendAsync("ReceiveMenuTableStatus", value);
+        }
     }
 }
